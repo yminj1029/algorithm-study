d1c70e4d53d36f23d7110f4c8d48297b8319b42f
MinjiYun <minjiyun204@gmail.com>
2021-05-18 21:01:57 +0900

    코드업 30번까지

---
+++ codeup/change.py
@@ -0,0 +1,8 @@
+num = int(input())
+print('%x' % num)  # x는 16진수 hex
+
+num1 = int(input())
+print('%X' % num1)  # 16진수 대문자는 대문자로
+
+num2 = int(input(), 16)  # 바로 16진수로 인식시키는 법
+print('%o' % num2)  # 8진수는 octo o
